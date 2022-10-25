0757cc3f6684e8ec27d0ac6d35c2821e2aba4f6d
Ekta Bharti <ektabharti019@gmail.com>
2022-10-25 13:54:17 -0400

    index file

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 //console.log("first commit")
-const logNumbers=()=>{
-    for(let i=1;i<=20;i++){
-        console.log(i)
+// const logNumbers=()=>{
+//     for(let i=1;i<=20;i++){
+//         console.log(i)
+//     }
+// }
+// logNumbers()
+const getMyNumbers = () =>{
+    let num;
+        for(let i=1; i<101; i++){
+            num=i;
+        
+        if(num%3===0 && num%5===0){
+            console.log(`FizzBuzz`);
+        }
+        else if(num%3===0){
+            console.log(`Fizz`);
+        }
+        else if(num%5===0){
+            console.log(`Buzz`);
+        }
+        else {
+            console.log(num);
+        }
     }
 }
-logNumbers()
+getMyNumbers();
